4ee057769b20798b233e065c6269222e2368fdf0
Gabriel <gabriel.zima@gmail.com>
2018-11-08 14:24:47 +0100

    Update CheckDomoticzAndRestart.sh

--- bash/CheckDomoticzAndRestart.sh
+++ bash/CheckDomoticzAndRestart.sh
@@ -2,6 +2,7 @@
 
 # Sprawdzanie czy Domoticz działa porprawnie (odpowiada po http jsonem) i jak nie to restart usługi
 # uwzględniony jest brak internetu aby nie restartować niepotrzebnie usługi z tego powodu.
+# Należy ten skrypt uruchamiać cyklicznie (CRON).
 
 # pobieramy lokalne IP aby go użyć w CURL
 LOCALIP=$(hostname -I | cut -d' ' -f1)
