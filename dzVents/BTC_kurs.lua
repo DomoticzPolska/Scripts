363ff00119174c5eb14877d7fbe2c4ddb6a484b7
Gabriel <gabriel.zima@gmail.com>
2018-11-08 13:43:22 +0100

    Dodanie polskiego opisu

--- dzVents/BTC_kurs.lua
+++ dzVents/BTC_kurs.lua
@@ -1,4 +1,7 @@
 -- Pobranie kursu BITCOIN z API https://www.bitmarket.pl/json/BTCPLN/ticker.json i aktualizacja urządzenia 'Kurs BTC'
+-- API zwraca następującego JSONa 
+-- {"ask":24328.5085,"bid":24320.0000,"last":24328.5085,"low":24194.0864,"high":24517.2600,"vwap":24414.8269,"volume":52.26252419}
+-- pobieramy z niego wartość dla "last", którą mamy w obiekcie item.json.last
 
 return {
     on = {
@@ -6,7 +9,7 @@ return {
         httpResponses = { 'BTCPLN' }
     },
     execute = function(domoticz, item)
-        if (item.isTimer) then
+        if (item.isTimer) then -- gdy funkcja wywołana jest za pomocą triggera "timer"
             domoticz.openURL({
                 url = 'https://www.bitmarket.pl/json/BTCPLN/ticker.json',
                 method = 'GET',
@@ -14,8 +17,8 @@ return {
             })
         end
 
-        if (item.isHTTPResponse and item.ok) then
-            domoticz.devices('Kurs BTC').updateCustomSensor(item.json.last)
+        if (item.isHTTPResponse and item.ok) then -- gdy funkcja wywołana jest za pomocą triggera "httpResponses"
+            domoticz.devices('Kurs BTC').updateCustomSensor(item.json.last) -- aktualizacja urządzenia
         end
     end
 }
