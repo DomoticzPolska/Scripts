d2fbb813d06b5692de0609c2e292d7fe749e939a
Gabriel <gabriel.zima@gmail.com>
2018-11-08 19:03:52 +0100

    Create tablica_rejestracyjna.sh

---
+++ bash/tablica_rejestracyjna.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# skrypt rozpoznaje tablicę rejestracyjną ze zdjęcia i zamienia jej numer na tekst.
+# jako parametr trzeba podać ścieżkę do zdjęcia.
+#
+# do poprawnego działania należy zainstalować programy:
+# 1. alpr (sudo apt install OpenALPR)
+# 2. jq (sudo apt install jq)
+
+
+plik_graficzny=%1
+
+rejestracja=$(alpr -c eu -n 1 -j $plik_graficzny | jq -r '.results[0].plate')
+
+echo $plik_graficzny
