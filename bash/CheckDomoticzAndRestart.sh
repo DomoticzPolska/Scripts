35656f175923182dbd43610d9c825bf4d48f11ed
Gabriel <gabriel.zima@gmail.com>
2018-11-08 14:24:09 +0100

    Create CheckDomoticzAndRestart.sh

---
+++ bash/CheckDomoticzAndRestart.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Sprawdzanie czy Domoticz działa porprawnie (odpowiada po http jsonem) i jak nie to restart usługi
+# uwzględniony jest brak internetu aby nie restartować niepotrzebnie usługi z tego powodu.
+
+# pobieramy lokalne IP aby go użyć w CURL
+LOCALIP=$(hostname -I | cut -d' ' -f1)
+
+# sprawdzamy status domoticza - czy odpowiada JSONem
+status=$(curl -k -s "http://$LOCALIP/json.htm?type=command&param=getconfig" | jq -r '.status')
+
+# sprawdzamy czy RPI ma internet
+check_internet=$(ping -q -w1 -c1 google.com &>/dev/null && echo online || echo offline)
+
+# sprawdzamy warunki i jeśli są spełnione to restartujemy usługę.
+if [ "OK" != $status ] && [ "offline" != $check_internet ] ; then
+    /etc/init.d/domoticz.sh restart
+fi
