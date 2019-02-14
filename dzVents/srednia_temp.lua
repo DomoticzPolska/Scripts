5a5f8504fef6eaf74667835f6fc7d14c401861b2
Gabriel <gabriel.zima@gmail.com>
2019-02-14 09:52:43 +0100

    Update srednia_temp.lua

--- dzVents/srednia_temp.lua
+++ dzVents/srednia_temp.lua
@@ -1,20 +1,23 @@
+-- Oplicza średnią temperaturę z czujników zdefinowanych w obiekcie data.temps
+-- i wyświetla ją w urządzeniu 'Temperatura Średnia'
+
 return {
 	on = {timer = {'every minute'}},
-	 data = {
-        temps = {
+	data = {
+        	temps = {
 			'Pokój Szymka',
 			'Pokój Marysi',
 			'Gabinet',
 			'Sypialnia'
 		}
-    },
+  },
 	execute = function(domoticz, device)
-	    local sum = 0
-	    local count = 0
-        for i, temp in pairs(domoticz.data.temps) do
-            sum = sum + domoticz.devices(temp).temperature
-            count = count + 1
-        end
-        domoticz.devices('Temperatura Średnia').updateTemperature(domoticz.round(sum / count, 1))
+    local sum = 0
+	  local count = 0
+      for i, temp in pairs(domoticz.data.temps) do
+          sum = sum + domoticz.devices(temp).temperature
+          count = count + 1
+      end
+      domoticz.devices('Temperatura Średnia').updateTemperature(domoticz.round(sum / count, 1))
 	end
 }
